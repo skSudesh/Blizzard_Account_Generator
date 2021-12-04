bc171f3d380ff57fcfba2512da3dd3f4404c12df
skSudesh <95521782+skSudesh@users.noreply.github.com>
2021-12-04 13:47:17 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+## Put your API key here
+anycaptcha_API_key = ""
+
+## These info will be used to generate accounts. You can change name, DOB as per your choice.
+first_name = "Jhone"
+last_name = "Kite"
+
